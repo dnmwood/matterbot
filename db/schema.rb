a0e89f45b898ca6a0503803364225efa031997b5
David Wood <davidnmwood@gmail.com>
2020-07-30 15:54:57 -0400

    generate subscription class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200730194546) do
+ActiveRecord::Schema.define(version: 20200730195427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20200730194546) do
     t.index ["cause_id"], name: "index_organizations_on_cause_id", using: :btree
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -57,4 +65,5 @@ ActiveRecord::Schema.define(version: 20200730194546) do
   end
 
   add_foreign_key "organizations", "causes"
+  add_foreign_key "subscriptions", "users"
 end
