5b7bda53e09e352b9f945482b28e68b7738d5777
David Wood <davidnmwood@gmail.com>
2020-07-30 15:44:18 -0400

    DB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200730193451) do
+ActiveRecord::Schema.define(version: 20200730194203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "causes", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,4 +34,15 @@ ActiveRecord::Schema.define(version: 20200730193451) do
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "organizations", force: :cascade do |t|
+    t.integer  "cause_id"
+    t.string   "title"
+    t.string   "description"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["cause_id"], name: "index_organizations_on_cause_id", using: :btree
+  end
+
+  add_foreign_key "organizations", "causes"
 end
