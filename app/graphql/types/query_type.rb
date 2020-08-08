e55cd54280bd1f48201875631d409476bb990486
David Wood <davidnmwood@gmail.com>
2020-08-08 13:44:28 -0400

    We got there.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,5 +13,20 @@ module Types
     def user(id:)
       User.find(id)
     end
+
+    field :organizations, [Types::OrganizationType], null: false # get organizations
+ 
+    def organizations
+      Organization.all
+    end
+
+    field :organization, [Types::OrganizationType], null: false do
+      argument :id, ID, required: true
+    end
+
+    def organization(id:)
+      Organization.find(id)
+    end
+
   end
 end
